325b0818a41d1021c3943f0435a2ed99855c5444
adham cahyo n <79594648+alubeban@users.noreply.github.com>
2021-11-21 18:08:41 +0700

    Add files via upload

---
+++ ServiceMTK-P1-066/ServiceMTK-P1-066/Matematika.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceMTK_P1_066
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Matematika : IMatematika
+    {
+        public int Bagi(int a, int b)
+        {
+            try
+            {
+                return a / b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //secara default 
+                //throw new exeption(ex.message) //pesan error seccara default
+                //throw new FaultException("pesan salah") // untuk meloloskan pesan yang dikirim ke client
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+
+        public int Kali(int a, int b)
+        {
+            try
+            {
+                return a * b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //secara default 
+                //throw new exeption(ex.message) //pesan error seccara default
+                //throw new FaultException("pesan salah") // untuk meloloskan pesan yang dikirim ke client
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+
+        public int Kurang(int a, int b)
+        {
+            try
+            {
+                return a - b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //secara default 
+                //throw new exeption(ex.message) //pesan error seccara default
+                //throw new FaultException("pesan salah") // untuk meloloskan pesan yang dikirim ke client
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+
+        public int Tambah(int a, int b)
+        {
+            try
+            {
+                return a + b;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //secara default 
+                //throw new exeption(ex.message) //pesan error seccara default
+                //throw new FaultException("pesan salah") // untuk meloloskan pesan yang dikirim ke client
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan yang anda masukkan salah";
+                mf.Pesan = "Masukkan input yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+
+        public Koordinat TKoordinat(Koordinat a, Koordinat b)
+        {
+            try
+            {
+                Koordinat hasil = new Koordinat();
+                hasil.X = a.X + b.X;
+                hasil.Y = a.Y + b.Y;
+                return hasil;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                MathFault mf = new MathFault();
+                mf.Kode = "Inputan Yang anda Masukkan Salah";
+                mf.Pesan = "Masukkan inputan yang benar";
+                throw new FaultException<MathFault>(mf);
+            }
+        }
+    }
+}
